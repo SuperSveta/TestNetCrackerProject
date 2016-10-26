b14c348f5ed7e941dfabb70a1be08e6831ba3173
SuperSveta <zmmlrz@gmail.com>
2016-10-26 23:00:43 +0300

    Super

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,6 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, New World!");
+        System.out.println("Hello, Super New World!");
     }
 }
